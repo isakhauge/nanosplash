45bfa2e01fe23fead2b0b2cd3880594e554f59af
Isak Hauge <isak.hauge@fdvhuset.no>
2021-08-11 19:14:02 +0200

    Make single file bundle

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import {defineConfig} from "vite";
+import {viteSingleFile} from "vite-plugin-singlefile";
 
 export default defineConfig({
+    plugins: [viteSingleFile()],
     build: {
         lib: {
             entry: 'src/main.ts',
             name: 'TinyLoader',
             fileName: 'tiny-loader',
-            formats: ['iife']
-        }
+            formats: ['iife'],
+        },
+        cssCodeSplit: true,
+        assetsInlineLimit: Infinity,
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+            },
+        },
+        minify: 'terser'
     }
 })
