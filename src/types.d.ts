357a815b2b4ef39222a50c2bb0a5bd0c81d5ea1d
Isak Hauge <isak.hauge@fdvhuset.no>
2021-08-12 18:54:03 +0200

    Update main types. Add class to module.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,52 +1,86 @@
-import {NanoSplash} from './NanoSplash'
-
 declare module 'nanosplash' {
-    type SplashAnimation = "none" | "pulse" | "spin";
-   
-    type DefaultOptions = {
-      destination?: Destination;
-      text?: string;
-    };
-    
-    type TextOptions = {
-      family?: string;
-      color?: string;
-      size?: string;
-    };
-    
-    type SplashOptions = {
-      src?: string;
-      width?: string;
-      animation?: SplashAnimation;
-    };
-    
-    type BackgroundOptions = {
-      color?: string;
-      blur?: boolean;
-    };
-    
-    type Destination = Node | Element | DestinationCallback | string;
-    
-    type DestinationCallback = () => Element;
-    
-    type Config = {
-      default?: DefaultOptions;
-      text?: TextOptions;
-      splash?: SplashOptions;
-      background?: BackgroundOptions;
-    };
-    
-    type DestinationController = {
-      inside: (destination: Destination) => void;
-    };
+  /**
+   * NanoSplash
+   *
+   * @author Isak K. Hauge
+   * @version 2.0
+   */
+  class NanoSplash {
+    constructor(config?: Config);
 
-    export type MakeOptions = {
-        id?: string
-        className?: string
-        attributes?: {key: string, value: string | null}[]
-        eventListeners?: {event: keyof HTMLElementEventMap, handler: EventListener}[],
-        content?: Node | Element | HTMLElement | string
-    }
+    /**
+     * Configure
+     *
+     * @param {Config} config
+     */
+    configure(config: Config): NanoSplash;
+
+    /**
+     * Show
+     *
+     * @param {string} text
+     */
+    show(text?: string): DestinationController;
+
+    /**
+     * Hide
+     */
+    hide(): void;
+
+    /**
+     * Inject Instance Into Global Scope
+     *
+     * @param {NanoSplash} instance
+     * @private
+     * @static
+     */
+    static injectInstanceIntoGlobalScope(instance: NanoSplash): void;
+  }
+
+  type SplashAnimation = "none" | "pulse" | "spin";
+  
+  type DefaultOptions = {
+    destination?: Destination;
+    text?: string;
+  };
+  
+  type TextOptions = {
+    family?: string;
+    color?: string;
+    size?: string;
+  };
+  
+  type SplashOptions = {
+    src?: string;
+    width?: string;
+    animation?: SplashAnimation;
+  };
+  
+  type BackgroundOptions = {
+    color?: string;
+    blur?: boolean;
+  };
+  
+  type Destination = Node | Element | DestinationCallback | string;
+  
+  type DestinationCallback = () => Element;
+  
+  type Config = {
+    default?: DefaultOptions;
+    text?: TextOptions;
+    splash?: SplashOptions;
+    background?: BackgroundOptions;
+  };
+  
+  type DestinationController = {
+    inside: (destination: Destination) => void;
+  };
+
+  type MakeOptions = {
+      id?: string
+      className?: string
+      attributes?: {key: string, value: string | null}[]
+      eventListeners?: {event: keyof HTMLElementEventMap, handler: EventListener}[],
+      content?: Node | Element | HTMLElement | string
+  }
 }
-
-export default NanoSplash
