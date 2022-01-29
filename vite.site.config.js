081534a45d85c9b9c5a37e6273782d017cd682d8
isakhauge <isak.hauge@fdvhuset.no>
2022-01-29 22:39:36 +0100

    Upgrade packages

--- vite.site.config.js
+++ vite.site.config.js
@@ -1,10 +1,7 @@
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
 	mode: 'production',
-	plugins: [vue(), viteSingleFile()],
 	build: {
 		lib: {
 			entry: 'index.html',
