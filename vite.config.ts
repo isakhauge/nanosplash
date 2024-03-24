ec724b46d6a4a56289f08f96d57f8ad571094ab1
Isak Hauge <isakhauge@icloud.com>
2024-03-24 20:13:59 +0100

    Added Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 import { LibraryFormats, defineConfig } from 'vite'
+import { codecovVitePlugin } from '@codecov/vite-plugin'
+import { config } from 'dotenv'
+config()
 
 const format = (process.env?.format ?? '') as LibraryFormats
 const entry = (process.env?.entry ?? '') as string
@@ -13,4 +16,11 @@ export default defineConfig({
 			fileName: 'ns',
 		},
 	},
+	plugins: [
+		codecovVitePlugin({
+			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+			bundleName: 'nanosplash',
+			uploadToken: process.env.CODECOV_TOKEN,
+		}),
+	],
 })
