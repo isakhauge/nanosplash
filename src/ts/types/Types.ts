041f433cc4e9ad174f809ddabf561f99ef370368
Isak Hauge <isakhauge@icloud.com>
2024-03-24 16:59:15 +0100

    Remove junk

--- src/ts/types/Types.ts
+++ src/ts/types/Types.ts
@@ -5,19 +5,6 @@ export type UTCInteger = Integer
 export type DOMSelector = string
 export type ElementReference = DOMSelector | Element
 
-export interface ScopedSelectors {
-	/**
-	 * @param selector Selector to match with a DOM element.
-	 * @returns An array of elements matching with the selector.
-	 */
-	all: (selector: DOMSelector) => Element[]
-	/**
-	 * @param selector Selector to match with a DOM element.
-	 * @returns Null or the first element in the DOM to match the selector.
-	 */
-	first: (selector: DOMSelector) => Element | null
-}
-
 export interface NSElement extends HTMLDivElement {
 	nsId: number
 }
