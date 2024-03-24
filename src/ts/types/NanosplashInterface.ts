18c0a030e0b50328643bd191659d5e732622a63d
Isak Hauge <isakhauge@icloud.com>
2024-03-24 22:49:57 +0100

    Update interface

--- src/ts/types/NanosplashInterface.ts
+++ src/ts/types/NanosplashInterface.ts
@@ -4,11 +4,14 @@ export interface NanosplashInterface {
 	/**
 	 * # Show
 	 * Display Nanosplash inside the window.
-	 * @param {string | undefined} text Optional text
+	 * @param {string | null | undefined} text Optional text. Pass falsy to display spinner only.
 	 * @param {string | Element | undefined} inside Optional element reference
 	 * @returns {number | null} The internal ID of the created Nanosplash.
 	 */
-	show(text?: string, inside?: DOMSelector | HTMLElement): UTCInteger | null
+	show(
+		text?: string | null | undefined,
+		inside?: DOMSelector | HTMLElement
+	): UTCInteger | null
 	/**
 	 * # Hide
 	 * Remove Nanosplash in multiple ways depending on the passed argument.
