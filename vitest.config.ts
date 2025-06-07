e2cad95cd514cf0bd48642ce4fcfdfc6e8b02be8
Isak Hauge <isakhauge@icloud.com>
2025-06-07 23:35:22 +0200

    Update workflow and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,12 @@ import path from "node:path";
 export default defineConfig({
     test: {
         environment: 'jsdom',
+        coverage: {
+            enabled: true,                      // Enable coverage collection
+            provider: 'v8',                     // Use 'v8' (default) or 'istanbul' provider
+            reporter: ['lcov'],                 // Output coverage in LCOV format
+            reportsDirectory: './coverage',     // Directory for coverage reports
+        },
     },
     resolve: {
         alias: {
